c69e2659a3c3785a8da422e055f1132a99ba5360
ibiloyeolusegun1 <ibiloyeolusegun.1@gmail.com>
2023-10-06 05:02:45 +0100

    Added Contact.jsx and modified App.jsx, main.css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Services from './components/Services'
 import Testimonial from './components/Testimonial'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <About/>
       <Services/>
       <Testimonial/>
+      <Contact/>
     </>
   )
 }
