df8c4d073a664da5e27a6ec8b716f208e5b810c9
ibiloyeolusegun1 <ibiloyeolusegun.1@gmail.com>
2023-10-08 23:51:01 +0100

    Updated footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Services from './components/Services'
+import Testimonial from './components/Testimonial'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Hero/>
+      <About/>
+      <Services/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
     </>
   )
 }
